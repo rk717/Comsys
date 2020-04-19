1df965284c3edaf9c14594aaac3cdb9c0a886d1c
rk717 <55735239+rk717@users.noreply.github.com>
2020-04-19 20:13:23 +0200

    Update 逻辑运算符.sh

--- shellscript/逻辑运算符.sh
+++ shellscript/逻辑运算符.sh
@@ -10,3 +10,18 @@ then
     else
     echo "not permit"
 fi
+
+
+---------------------------------------
+#!/bin/bash
+
+
+
+num1=20
+num2=5
+
+echo $(( num1 + num2 )) # $(expr $num1 + $num2 )
+echo $(( num1 - num2 )) # $(expr $num1 - $num2 )
+echo $(( num1 * num2 )) # $(expr $num1 \* $num2 )
+echo $(( num1 / num2 )) # $(expr $num1 / $num2 )
+echo $(( num1 % num2 )) # $(expr $num1 % $num2 )
