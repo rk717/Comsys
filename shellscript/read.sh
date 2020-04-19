3dee187cacd98121a821351f23e743a7295be4b5
rk717 <55735239+rk717@users.noreply.github.com>
2020-04-19 15:03:23 +0200

    Update read.sh

--- shellscript/read.sh
+++ shellscript/read.sh
@@ -13,3 +13,24 @@ echo
 
 
 echo "account: $acc  password: $pw  "
+
+
+-------------------------------------------------
+#!/bin/bash
+
+#read user input
+# echo "Enter name: "
+# read name1 name2 name3
+# echo "Enter name: $name1, $name2, $name3"
+
+# read -p 'username: ' user_var
+# read -sp 'password: ' pass_var
+# echo
+# echo "username: $user_var"
+# echo "password: $pass_var"
+
+echo "Enter names: "
+read -a names
+echo "Names: ${names[0]}, ${names[1]}"
+
+
