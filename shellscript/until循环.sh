5307a1157de6e01a84f229badb5edf47c5d144ba
rk717 <55735239+rk717@users.noreply.github.com>
2020-04-19 20:58:09 +0200

    Update until循环.sh

--- shellscript/until循环.sh
+++ shellscript/until循环.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+#!/bin/bash
+
+#until loop
+
+
+# until [ condition ]
+# do 
+#     command1
+#     command2
+#     ...
+#     ...
+#     commandN
+# done
+
+n=1
+
+until [ $n -ge 10 ] #(( $n > 10 ))
+do 
+    echo $n 
+    n=$(( n+1 ))
+done
+-------------------------------------------------
 a1(){
 num=1
 until [ $num -gt 20 ];
