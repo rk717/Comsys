445204df17f926ed0cebf5c5743526f99ee632a5
rk717 <55735239+rk717@users.noreply.github.com>
2020-04-19 15:46:10 +0200

    Update file.sh

--- shellscript/file.sh
+++ shellscript/file.sh
@@ -1,3 +1,24 @@
+在存在的file 里 写 内容
+#!/bin/bash
+
+echo -e "Enter the name of the file: \c" #-e, 加了这个，字符串里的\c 才可以使用
+read file_name
+
+if [ -f $file_name ]  #-f file, -d directory, -s 查看file里面是不是空的 , -r check if the file has read permission, -w, -e
+then
+    if [ -w $file_name ]
+    then 
+        echo "type some text data. To quit press ctrl+d"
+        cat >> $file_name
+    else
+        echo "file do not have write permission"
+    fi
+else
+    echo "$file_name not exist"
+fi
+
+--------------------------------------------------
+
 #!/bin/bash
 
 echo -e "Enter the name of the file: \c" #-e, 加了这个，字符串里的\c 才可以使用
