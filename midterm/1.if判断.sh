f186e7f97abdad1299e2c68895514b012407c675
rk717 <55735239+rk717@users.noreply.github.com>
2020-04-21 08:00:42 +0200

    Update 1.if判断.sh

--- midterm/1.if判断.sh
+++ midterm/1.if判断.sh
@@ -7,3 +7,15 @@ if (( $1*$2<100 ));then
 else
     echo "not less"
 fi    
+
+--------------------------------------------------------------
+#!/bin/bash
+#the script should determinate if the first parameter is in the given interval
+#First parameter should be the number
+#Second and third should define the interval
+
+if [ $1 -ge $2 -a $1 -le $3 ]; then
+    echo "The given number ($1) is between $2 and $3"
+else
+    echo "The given number is outside of the given interval ($2-$3)"
+fi
